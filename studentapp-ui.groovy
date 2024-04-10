ebde9a632a494ba8021ac8a546e87b6c3aabab97
omkeo <152286404+omkeo@users.noreply.github.com>
2024-04-10 13:30:29 +0530

    Update studentapp-ui.groovy

--- studentapp-ui.groovy
+++ studentapp-ui.groovy
@@ -16,7 +16,10 @@ pipeline {
         }
         stage('Test') {
             steps {
-                
+                sh '''/opt/apache-maven-3.9.6/bin/mvn sonar:sonar \\
+                        -Dsonar.projectKey=student \\
+                        -Dsonar.host.url=http://13.208.169.87:9000 \\
+                        -Dsonar.login=d1443564cb1ff774fc8e208c89fe9ab4bef53743'''
             }
         }
         stage('Quality Test') {
